6f4ab0e9958e2f50ff66b461259b11c5945a995a
Andrei Crișan <109060372+andrei-crisan@users.noreply.github.com>
2023-01-23 19:39:10 +0200

    Refactorizare: negRST v1.0.1 (settings) (#8)

--- src/main/java/com/neg/NegApp.java
+++ src/main/java/com/neg/NegApp.java
@@ -12,7 +12,6 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
