616831ad41bf54edc194d28192202523d822d266
Andrei Crișan <109060372+andrei-crisan@users.noreply.github.com>
2023-01-23 16:07:33 +0200

    refactorizare cod via gradle (#1)

---
+++ src/main/java/com/neg/NegApp.java
@@ -0,0 +1,191 @@
+package com.neg;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+public class NegApp extends Application {
+    Engine engine = new Engine();
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Path pathFisier = Paths.get("C:\\Users\\" + System.getenv("username") + "\\AppData\\Roaming\\Adobe\\Lightroom\\Preferences\\Lightroom Classic CC 7 Preferences.agprefs");
+        Map<String, String> stringuriCandidate = new HashMap<String, String>();
+        stringuriCandidate.put("	photosAppliedV4 = 12,\\", "	photosAppliedV4 = 0,\\");
+        stringuriCandidate.put("	photosAppliedV4 = 11,\\", " 	photosAppliedV4 = 0,\\");
+        stringuriCandidate.put("	photosAppliedV4 = 10,\\", " 	photosAppliedV4 = 0,\\");
+        stringuriCandidate.put("	photosAppliedV4 = 9,\\", " 	photosAppliedV4 = 0,\\");
+        stringuriCandidate.put("	photosAppliedV4 = 8,\\", " 	photosAppliedV4 = 0,\\");
+        stringuriCandidate.put("	photosAppliedV4 = 7,\\", " 	photosAppliedV4 = 0,\\");
+        stringuriCandidate.put("	photosAppliedV4 = 6,\\", " 	photosAppliedV4 = 0,\\");
+        stringuriCandidate.put("	photosAppliedV4 = 5,\\", " 	photosAppliedV4 = 0,\\");
+        stringuriCandidate.put("	photosAppliedV4 = 4,\\", " 	photosAppliedV4 = 0,\\");
+        stringuriCandidate.put("	photosAppliedV4 = 3,\\", " 	photosAppliedV4 = 0,\\");
+        stringuriCandidate.put("	photosAppliedV4 = 2,\\", " 	photosAppliedV4 = 0,\\");
+        stringuriCandidate.put("	photosAppliedV4 = 1,\\", " 	photosAppliedV4 = 0,\\");
+
+        try {
+            Group root = new Group();
+            Scene scene = new Scene(root, 300, 150, Color.DARKOLIVEGREEN);
+            stage.setTitle("NegRST");
+            Image logo = new Image("logo.png");
+            stage.getIcons().add(logo);
+            stage.setResizable(false);
+
+            Rectangle incadrare = new Rectangle();
+            incadrare.setX(5);
+            incadrare.setY(5);
+            incadrare.setWidth(290);
+            incadrare.setHeight(140);
+            incadrare.setFill(Color.LIGHTSLATEGRAY);
+            incadrare.setStrokeWidth(1);
+            incadrare.setStroke(Color.BLACK);
+            root.getChildren().add(incadrare);
+
+            Image backgroundElem1 = new Image("symbol1.png");
+            ImageView imgViewbackgroundElem1 = new ImageView(backgroundElem1);
+            imgViewbackgroundElem1.setX(26);
+            imgViewbackgroundElem1.setY(25);
+            root.getChildren().add(imgViewbackgroundElem1);
+
+            Image backgroundElem2 = new Image("symbol2.png");
+            ImageView imgViewbackgroundElem2 = new ImageView(backgroundElem2);
+            imgViewbackgroundElem2.setX(226);
+            imgViewbackgroundElem2.setY(22);
+            root.getChildren().add(imgViewbackgroundElem2);
+
+            Image logoInsideBox = new Image("logo.png");
+            ImageView imgView = new ImageView(logoInsideBox);
+            imgView.setX(90);
+            imgView.setY(13);
+            root.getChildren().add(imgView);
+
+            Text titluApp = new Text();
+            Font fontTitluApp = Font.font("Verdana", FontWeight.BOLD, 12);
+            titluApp.setText("negative\n    RST");
+            titluApp.setFont(fontTitluApp);
+            titluApp.setFill(Color.LIGHTGRAY);
+            titluApp.setX(126);
+            titluApp.setY(75);
+            root.getChildren().add(titluApp);
+
+            Text disclamer = new Text();
+            Font disclamerFont = Font.font("Verdana", FontWeight.BOLD, 8);
+            disclamer.setText("for educational purpose");
+            disclamer.setFont(disclamerFont);
+            disclamer.setFill(Color.BLACK);
+            disclamer.setX(183);
+            disclamer.setY(133);
+            root.getChildren().add(disclamer);
+
+            Rectangle rectStatus = new Rectangle();
+            rectStatus.setY(135);
+            rectStatus.setX(10);
+            rectStatus.setFill(Color.BLACK);
+            rectStatus.setStrokeWidth(1);
+            rectStatus.setStroke(Color.BLACK);
+            rectStatus.setWidth(280);
+            rectStatus.setHeight(5);
+            root.getChildren().add(rectStatus);
+
+            Rectangle rectStatus2 = new Rectangle();
+            rectStatus2.setY(10);
+            rectStatus2.setX(10);
+            rectStatus2.setFill(Color.BLACK);
+            rectStatus2.setStrokeWidth(1);
+            rectStatus2.setStroke(Color.BLACK);
+            rectStatus2.setWidth(280);
+            rectStatus2.setHeight(5);
+            root.getChildren().add(rectStatus2);
+
+            Rectangle rectangleLabelContor = new Rectangle();
+            rectangleLabelContor.setY(55);
+            rectangleLabelContor.setX(15);
+            rectangleLabelContor.setFill(Color.WHITE);
+            rectangleLabelContor.setStrokeWidth(1);
+            rectangleLabelContor.setStroke(Color.BLACK);
+            rectangleLabelContor.setWidth(70);
+            rectangleLabelContor.setHeight(20);
+            root.getChildren().add(rectangleLabelContor);
+
+            Rectangle rectangleContor = new Rectangle();
+            rectangleContor.setY(75);
+            rectangleContor.setX(15);
+            rectangleContor.setFill(Color.BLACK);
+            rectangleContor.setStrokeWidth(1);
+            rectangleContor.setStroke(Color.BLACK);
+            rectangleContor.setWidth(70);
+            rectangleContor.setHeight(30);
+            root.getChildren().add(rectangleContor);
+
+            Text labelContor = new Text();
+            labelContor.setText("Negatives:");
+            labelContor.setX(23);
+            labelContor.setY(69);
+            root.getChildren().add(labelContor);
+
+            Text contor = new Text();
+            contor.setText(engine.counterFoto(pathFisier));
+            contor.setFill(Color.WHITE);
+            contor.setX(43);
+            contor.setY(93);
+            root.getChildren().add(contor);
+
+            Button reset = new Button();
+            reset.setText("Reset");
+            reset.setLayoutX(230);
+            reset.setLayoutY(50);
+            root.getChildren().add(reset);
+
+            Button ext = new Button();
+            ext.setText("  Exit ");
+            ext.setLayoutX(230);
+            ext.setLayoutY(80);
+            root.getChildren().add(ext);
+
+            ext.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent arg0) {
+                    engine.exitApp();
+                }
+            });
+            reset.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent arg0) {
+                    engine.resetCounter(pathFisier, stringuriCandidate);
+                    double parametru = 0;
+                    for (int i = 0; i <= 12; i++) {
+                        parametru += 20;
+                        root.getChildren().add(engine.loader(parametru));
+                        contor.setText(engine.counterFoto(pathFisier));
+                    }
+                }
+            });
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
